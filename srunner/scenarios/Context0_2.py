28dd28983fcd92c02f097746fc9ee7f9eba59b48
Galigne <broxarcane@gmail.com>
2021-12-07 16:09:43 +0100

    add All Scenarios for context0

--- srunner/scenarios/Context0_2.py
+++ srunner/scenarios/Context0_2.py
@@ -19,7 +19,7 @@ from srunner.scenariomanager.timer import TimeOut
 from srunner.scenarios.basic_scenario import BasicScenario
 from srunner.tools.scenario_helper import get_location_in_distance_from_wp
 
-class Context0(BasicScenario):
+class Context0_2(BasicScenario):
 
     """
     This class represent the Context0
@@ -31,23 +31,26 @@ class Context0(BasicScenario):
     """
 
     def __init__(self, world, ego_vehicles, config, randomize=False,
-                 debug_mode=False, criteria_enable=True, adversary_type=False, timeout=600):
+                 debug_mode=False, criteria_enable=True, timeout=1200):
         """
         Setup all relevant parameters and create scenario
         """
         self._wmap = CarlaDataProvider.get_map()
 
         self._reference_waypoint = self._wmap.get_waypoint(config.trigger_points[0].location)
+        
         # ego vehicle parameters
         self._ego_vehicle_distance_driven = 30
+        
         # other vehicle parameters
         self._other_actor_target_velocity = 5
         self._other_actor_max_brake = 1.0
         self._time_to_reach = 10
-        self._adversary_type = adversary_type  # flag to select either pedestrian (False) or cyclist (True)
         self._walker_yaw = 0
         self._num_lane_changes = 1
         self.transform = None
+        self.transform2 = None
+        
         self.timeout = timeout
         self._trigger_location = config.trigger_points[0].location
         # Total Number of attempts to relocate a vehicle before spawning
@@ -55,7 +58,7 @@ class Context0(BasicScenario):
         # Number of attempts made so far
         self._spawn_attempted = 0
         self._ego_route = CarlaDataProvider.get_ego_vehicle_route()
-        super(Context0, self).__init__("Context0", ego_vehicles, config,world, debug_mode, criteria_enable=criteria_enable)
+        super(Context0_2, self).__init__("Context0_2", ego_vehicles, config, world, debug_mode, criteria_enable=criteria_enable)
 
     def _calculate_base_transform(self, _start_distance, waypoint):
         lane_width = waypoint.lane_width
@@ -66,7 +69,7 @@ class Context0(BasicScenario):
             stop_at_junction = True
         location, _ = get_location_in_distance_from_wp(waypoint, _start_distance, stop_at_junction)
         waypoint = self._wmap.get_waypoint(location)
-        offset = {"orientation": 270, "position": 50, "z": 0.6, "k": 1.0}
+        offset = {"orientation": 290, "position": 50, "z": 0, "k": 1.0}
         position_yaw = waypoint.transform.rotation.yaw + offset['position']
         orientation_yaw = waypoint.transform.rotation.yaw + offset['orientation']
         offset_location = carla.Location(
@@ -75,21 +78,14 @@ class Context0(BasicScenario):
         location += offset_location
         location.z = self._trigger_location.z + offset['z']
         return carla.Transform(location, carla.Rotation(yaw=orientation_yaw)), orientation_yaw
-
+        
     def _spawn_adversary(self, transform, orientation_yaw):
 
         self._time_to_reach *= self._num_lane_changes
-
-        if self._adversary_type is False:
-            self._walker_yaw = orientation_yaw
-            self._other_actor_target_velocity = 3 + (0.4 * self._num_lane_changes)
-            walker = CarlaDataProvider.request_new_actor('walker.*', transform)
-            adversary = walker
-        else:
-            self._other_actor_target_velocity = self._other_actor_target_velocity * self._num_lane_changes
-            first_vehicle = CarlaDataProvider.request_new_actor('vehicle.diamondback.century', transform)
-            first_vehicle.set_simulate_physics(enabled=False)
-            adversary = first_vehicle
+        self._walker_yaw = orientation_yaw
+        self._other_actor_target_velocity = 3 + (0.4 * self._num_lane_changes)
+        walker = CarlaDataProvider.request_new_actor('walker.*', transform)
+        adversary = walker
 
         return adversary
 
@@ -101,7 +97,7 @@ class Context0(BasicScenario):
         # cyclist transform
         _start_distance = 12
         
-        # We start by getting and waypoint in the closest sidewalk.
+        # We start by getting a waypoint in the closest sidewalk.
         waypoint = self._reference_waypoint
         while True:
             wp_next = waypoint.get_right_lane()
@@ -122,6 +118,9 @@ class Context0(BasicScenario):
             try:
                 self.transform, orientation_yaw = self._calculate_base_transform(_start_distance, waypoint)
                 first_vehicle = self._spawn_adversary(self.transform, orientation_yaw)
+                
+                self.transform2 = carla.Transform(carla.Location(250, -243, self._trigger_location.z), carla.Rotation(yaw=250))
+                second_vehicle = CarlaDataProvider.request_new_actor('walker.*', self.transform2)
                 break
             except RuntimeError as r:
                 # We keep retrying until we spawn
@@ -132,14 +131,15 @@ class Context0(BasicScenario):
                     raise r
 
         # Now that we found a possible position we just put the vehicle to the underground
-        disp_transform = carla.Transform(
-            carla.Location(self.transform.location.x,
-                           self.transform.location.y,
-                           self.transform.location.z - 500),
-            self.transform.rotation)
-        first_vehicle.set_transform(disp_transform)
+        #disp_transform = carla.Transform(carla.Location(self.transform.location.x, self.transform.location.y, self.transform.location.z - 500), self.transform.rotation)
+        #first_vehicle.set_transform(disp_transform)
         first_vehicle.set_simulate_physics(enabled=False)
         self.other_actors.append(first_vehicle)
+        
+        #disp_transform2 = carla.Transform(carla.Location(self.transform2.location.x, self.transform2.location.y, self.transform2.location.z - 500), self.transform2.rotation)
+        #second_vehicle.set_transform(disp_transform2)
+        second_vehicle.set_simulate_physics(enabled=False)
+        self.other_actors.append(second_vehicle)
 
     def _create_behavior(self):
         """
@@ -161,12 +161,25 @@ class Context0(BasicScenario):
             start_condition = InTimeToArrivalToVehicle(self.ego_vehicles[0], self.other_actors[0], self._time_to_reach)
             
         actor_velocity = KeepVelocity(self.other_actors[0], self._other_actor_target_velocity, name="walker velocity")
+        actor_velocity2 = KeepVelocity(self.other_actors[1], self._other_actor_target_velocity, name="walker2 velocity")
+        
         actor_drive = DriveDistance(self.other_actors[0], 0.5 * lane_width, name="walker drive distance")
+        actor_drive2 = DriveDistance(self.other_actors[1], 0.5 * lane_width, name="walker2 drive distance")
+        
         actor_start_cross_lane = AccelerateToVelocity(self.other_actors[0], 1.0, self._other_actor_target_velocity, name="walker crossing lane accelerate velocity")
+        actor_start_cross_lane2 = AccelerateToVelocity(self.other_actors[1], 1.0, self._other_actor_target_velocity, name="walker2 crossing lane accelerate velocity")
+        
         actor_cross_lane = DriveDistance(self.other_actors[0], lane_width, name="walker drive distance for lane crossing ")
+        actor_cross_lane2 = DriveDistance(self.other_actors[1], lane_width, name="walker2 drive distance for lane crossing ")
+        
         actor_stop_crossed_lane = StopVehicle(self.other_actors[0], self._other_actor_max_brake, name="walker stop")
+        actor_stop_crossed_lane2 = StopVehicle(self.other_actors[1], self._other_actor_max_brake, name="walker2 stop")
+        
         ego_pass_machine = DriveDistance(self.ego_vehicles[0], 5, name="ego vehicle passed prop")
+        
         actor_remove = ActorDestroy(self.other_actors[0], name="Destroying walker")
+        actor_remove2 = ActorDestroy(self.other_actors[1], name="Destroying walker2")
+        
         end_condition = DriveDistance(self.ego_vehicles[0], self._ego_vehicle_distance_driven, name="End condition ego drive distance")
 
         # non leaf nodes
@@ -177,19 +190,28 @@ class Context0(BasicScenario):
         # building tree
         root.add_child(scenario_sequence)
         scenario_sequence.add_child(ActorTransformSetter(self.other_actors[0], self.transform, name='TransformSetterTS3walker'))
+        scenario_sequence.add_child(ActorTransformSetter(self.other_actors[1], self.transform2, name='TransformSetterTS3walker'))
         scenario_sequence.add_child(HandBrakeVehicle(self.other_actors[0], True))
+        scenario_sequence.add_child(HandBrakeVehicle(self.other_actors[1], True))
         scenario_sequence.add_child(start_condition)
         scenario_sequence.add_child(HandBrakeVehicle(self.other_actors[0], False))
+        scenario_sequence.add_child(HandBrakeVehicle(self.other_actors[1], False))
         scenario_sequence.add_child(keep_velocity)
         scenario_sequence.add_child(keep_velocity_other)
         scenario_sequence.add_child(actor_stop_crossed_lane)
+        scenario_sequence.add_child(actor_stop_crossed_lane2)
         scenario_sequence.add_child(actor_remove)
+        scenario_sequence.add_child(actor_remove2)
         scenario_sequence.add_child(end_condition)
 
         keep_velocity.add_child(actor_velocity)
+        keep_velocity.add_child(actor_velocity2)
         keep_velocity.add_child(actor_drive)
+        keep_velocity.add_child(actor_drive2)
         keep_velocity_other.add_child(actor_start_cross_lane)
+        keep_velocity_other.add_child(actor_start_cross_lane2)
         keep_velocity_other.add_child(actor_cross_lane)
+        keep_velocity_other.add_child(actor_cross_lane2)
         keep_velocity_other.add_child(ego_pass_machine)
         
         return root
